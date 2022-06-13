31a4e9a011800da13393920ed9d98e86681cca54
kamath7 <adithyakamath96@gmail.com>
2022-06-14 01:46:02 +0530

    some options added

--- WebScraping/app.py
+++ WebScraping/app.py
@@ -1,3 +1,9 @@
 from selenium import webdriver
+
 driver = webdriver.Chrome('./chromedriver.exe')
+options = webdriver.ChromeOptions()
+options.add_argument("disable-infobars") #disabling alerts/popups
+options.add_argument("start-maximized")
+options.add_argument('disable-dev-shm-usage') #to avoid other kinds of issues
+options.add_argument("no-sandbox") #to get the greater priveleges
 driver.get("https://google.com")
