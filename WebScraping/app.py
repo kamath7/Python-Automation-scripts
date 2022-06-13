bd573afa02aa3d1f3bd038318eb7e53570b88ab4
kamath7 <adithyakamath96@gmail.com>
2022-06-14 02:01:21 +0530

    making it headless

--- WebScraping/app.py
+++ WebScraping/app.py
@@ -6,17 +6,18 @@ def get_driver():
     options = webdriver.ChromeOptions()
     options.add_argument("disable-infobars") #disabling alerts/popups
     options.add_argument("start-maximized")
+    options.add_argument("--headless")
     options.add_argument('disable-dev-shm-usage') #to avoid other kinds of issues
     options.add_argument("no-sandbox") #to get the greater priveleges
     options.add_experimental_option("excludeSwitches",["enable-automation"])  #to avoid detection
-    options.add_experimental_option("disable-blink-features=AutomationControlled")
+    options.add_argument("disable-blink-features=AutomationControlled")
     driver = webdriver.Chrome('./chromedriver.exe', options=options)
     driver.get('https://automated.pythonanywhere.com/')
     return driver
 
 def main():
     driver = get_driver()
-    element = driver.find_element_by_xpath('/html/body/div[1]/div/h1[1]')
-    return element
+    element = driver.find_element(by='xpath',value='/html/body/div[1]/div/h1[1]')
+    return element.text
 
-
+print(main())
