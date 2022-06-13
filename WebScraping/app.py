57515167d6b834bb264ed06f74df6a589fe5c182
kamath7 <adithyakamath96@gmail.com>
2022-06-14 01:48:12 +0530

    couple of more options

--- WebScraping/app.py
+++ WebScraping/app.py
@@ -6,4 +6,8 @@ options.add_argument("disable-infobars") #disabling alerts/popups
 options.add_argument("start-maximized")
 options.add_argument('disable-dev-shm-usage') #to avoid other kinds of issues
 options.add_argument("no-sandbox") #to get the greater priveleges
+options.add_experimental_option("excludeSwitches",["enable-automation"])  #to avoid detection
+options.add_experimental_option("disable-blink-features=AutomationControlled")
+
+
 driver.get("https://google.com")
