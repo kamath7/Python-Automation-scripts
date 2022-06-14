8c4194708cae683fee528d6c5e69ba77ed606a32
kamath7 <adithyakamath96@gmail.com>
2022-06-15 00:27:46 +0530

    Login automated

--- WebScraping/app2.py
+++ WebScraping/app2.py
@@ -1,28 +1,29 @@
+#clicking on buttons and adding input
+
 from selenium import webdriver
 import time
+from selenium.webdriver.common.keys import Keys 
 
 def get_driver():
     
     options = webdriver.ChromeOptions()
     options.add_argument("disable-infobars") #disabling alerts/popups
     options.add_argument("start-maximized")
-    options.add_argument("--headless")
+    # options.add_argument("--headless")
     options.add_argument('disable-dev-shm-usage') #to avoid other kinds of issues
     options.add_argument("no-sandbox") #to get the greater priveleges
     options.add_experimental_option("excludeSwitches",["enable-automation"])  #to avoid detection
     options.add_argument("disable-blink-features=AutomationControlled")
     driver = webdriver.Chrome('./chromedriver.exe', options=options)
-    driver.get('https://automated.pythonanywhere.com/')
+    driver.get('https://automated.pythonanywhere.com/login')
     return driver
-
-def cleanText(text):
-    return float(text.split(': ')[1])
 
 def main():
     driver = get_driver()
-    element = driver.find_element(by='xpath',value='/html/body/div[1]/div/h1[1]')
-    time.sleep(2)
-    element2 = driver.find_element(by='xpath', value='/html/body/div[1]/div/h1[2]')
-    return element.text,cleanText(element2.text)
+    driver.find_element(by="id", value="id_username").send_keys("automated") #env in the future
+    time.sleep(3)
+    driver.find_element(by="id", value="id_password").send_keys("automatedautomated"+ Keys.RETURN)
+    driver.current_url #to stop window from closing
+
 
 print(main())
