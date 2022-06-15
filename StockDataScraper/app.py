34cb79c23c8fb2f54e3f43421f66793422b5b8b2
kamath7 <adithyakamath96@gmail.com>
2022-06-15 21:06:42 +0530

    providing Tickets/From date and ToDate

--- StockDataScraper/app.py
+++ StockDataScraper/app.py
@@ -1,6 +1,18 @@
 import requests
+from datetime import datetime
+import time 
 
-url = "https://query1.finance.yahoo.com/v7/finance/download/AMZN?period1=863740800&period2=1655251200&interval=1d&events=history&includeAdjustedClose=true"
+def convert_to_epoch(text):
+    return int(time.mktime(datetime.strptime(text, '%Y/%m/%d').timetuple()))
+
+from_date = input("Enter start date (yyyy/mm/dd): ")
+to_date = input("Enter end date (yyyy/mm/dd): ")
+ticker = input("Enter ticker (MSFT/AMZN/GOOG/APPL/META")
+
+from_date = convert_to_epoch(from_date)
+to_date = convert_to_epoch(to_date)
+
+url = f"https://query1.finance.yahoo.com/v7/finance/download/{ticker}?period1={from_date}&period2={to_date}&interval=1d&events=history&includeAdjustedClose=true"
 headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36"} 
 content = requests.get(url, headers=headers).content
 
