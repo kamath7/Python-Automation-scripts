399b78fc069156cfbd4619075afb9b72cc844d8f
kamath7 <adithyakamath96@gmail.com>
2022-06-15 20:53:34 +0530

    saving to file

--- StockDataScraper/app.py
+++ StockDataScraper/app.py
@@ -3,4 +3,6 @@ import requests
 url = "https://query1.finance.yahoo.com/v7/finance/download/AMZN?period1=863740800&period2=1655251200&interval=1d&events=history&includeAdjustedClose=true"
 headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36"} 
 content = requests.get(url, headers=headers).content
-print(content)
+
+with open("data.csv", 'wb') as file:
+    file.write(content)
