665a0042834f566e235a3cd41be742b52120ebca
kamath7 <adithyakamath96@gmail.com>
2022-06-17 01:36:27 +0530

    personalised news

--- NewsGuy/app.py
+++ NewsGuy/app.py
@@ -9,5 +9,21 @@ api_key = os.getenv('NEWS_KEY_API')
 r = requests.get(f'https://newsapi.org/v2/everything?qInTitle=Manchester%20United&from=2022-5-16&to=2022-6-15&sortBy=popularity&language=en&apiKey={api_key}')
 content = r.json()
 my_articles = content['articles']
-for i in range(0, len(my_articles)):
-    print(f"${my_articles[i]['title']} -> ${my_articles[i]['description']} \n")
+# for i in range(0, len(my_articles)):
+#     print(f"${my_articles[i]['title']} -> ${my_articles[i]['description']} \n")
+
+#better loop
+# for article in my_articles:
+#     print(article['title'],' | ', article['description'],'\n')
+
+#personal news
+
+def get_my_news(topic, from_date, to_date, language='en'):
+    r = requests.get(f'https://newsapi.org/v2/everything?qInTitle={topic}&from={from_date}&to={to_date}&sortBy=popularity&language=en&apiKey={api_key}')
+    content = r.json()
+    my_articles = content['articles']
+    for article in my_articles:
+        print(article['title'],' | ', article['description'],'\n')
+
+
+get_my_news('Linkin Park','2022-06-01','2022-06-15')
