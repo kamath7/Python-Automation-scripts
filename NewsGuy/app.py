003a9295e858cf96a21b07c5ec372aa1ae828fc3
kamath7 <adithyakamath96@gmail.com>
2022-06-17 00:52:37 +0530

    request to api

--- NewsGuy/app.py
+++ NewsGuy/app.py
@@ -1,7 +1,11 @@
 import os 
 from dotenv import load_dotenv
+import requests
 
 load_dotenv()
 
 api_key = os.getenv('NEWS_KEY_API')
-print(api_key)
+
+r = requests.get(f'https://newsapi.org/v2/everything?qInTitle=stock%20market&from=2022-5-16&to=2022-6-15&sortBy=popularity&language=en&apiKey={api_key}')
+content = r.json()
+print(content)
