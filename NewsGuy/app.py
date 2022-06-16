4e7b1490258d530c79319e6b666bba9e03489f8d
kamath7 <adithyakamath96@gmail.com>
2022-06-17 00:58:03 +0530

    man utd news to make me cry

--- NewsGuy/app.py
+++ NewsGuy/app.py
@@ -6,8 +6,8 @@ load_dotenv()
 
 api_key = os.getenv('NEWS_KEY_API')
 
-r = requests.get(f'https://newsapi.org/v2/everything?qInTitle=stock%20market&from=2022-5-16&to=2022-6-15&sortBy=popularity&language=en&apiKey={api_key}')
+r = requests.get(f'https://newsapi.org/v2/everything?qInTitle=Manchester%20United&from=2022-5-16&to=2022-6-15&sortBy=popularity&language=en&apiKey={api_key}')
 content = r.json()
 my_articles = content['articles']
 for i in range(0, len(my_articles)):
-    print(my_articles[i]['title'])
+    print(f"${my_articles[i]['title']} -> ${my_articles[i]['description']} \n")
