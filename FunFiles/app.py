999c90f5c2db7b400c40862816100fe9aca41902
kamath7 <adithyakamath96@gmail.com>
2022-06-27 22:28:22 +0530

    printing filename

--- FunFiles/app.py
+++ FunFiles/app.py
@@ -2,7 +2,14 @@
 from pathlib import Path
 
 p1 = Path('./abc.txt')
+p2 = Path('./gef.txt')
 
 if p1.exists():
     with open('abc.txt') as f1:
         print(f1.read())
+
+if not p2.exists():
+    with open(p2,'w') as f1:
+        f1.write('written through pathlib')
+
+print(p1.name)
