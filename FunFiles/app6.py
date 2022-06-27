1e4c4a8773e498e9079a7c0aecffbd3f0f2698e7
kamath7 <adithyakamath96@gmail.com>
2022-06-28 00:38:09 +0530

    yeeting the files to a zip

--- FunFiles/app6.py
+++ FunFiles/app6.py
@@ -8,4 +8,4 @@ archive_path = root_dir / Path('archive.zip')
 with zipfile.ZipFile(archive_path, 'w') as zf:
     for path in root_dir.rglob('*.txt'):
         zf.write(path)
-        
+        path.unlink()
