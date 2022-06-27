cca8e392ddb8a604b080fcb9a70ad47c6a0f190a
kamath7 <adithyakamath96@gmail.com>
2022-06-28 00:25:27 +0530

    rename

--- FunFiles/app4.py
+++ FunFiles/app4.py
@@ -4,4 +4,9 @@ root_dir = Path('myawesomedirectory')
 # file_paths = root_dir.iterdir()
 file_paths = root_dir.glob('**/*')
 for path in file_paths:
-    print(path)
+    if path.is_file():
+        parent_fol = path.parts[-2] #splits path ('myawesomedi','lalle1','1.txt')
+        new_filename = parent_fol + 'morelalle' + path.name
+        print(new_filename)
+        new_filepath = path.with_name(new_filename)
+        path.rename(new_filepath)
