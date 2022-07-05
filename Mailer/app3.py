e1f38e8694be45700ba1d1d39a39ecaea766f2d6
kamath7 <adithyakamath96@gmail.com>
2022-07-05 23:46:17 +0530

    sends it

--- Mailer/app3.py
+++ Mailer/app3.py
@@ -5,17 +5,18 @@ import yagmail
 
 load_dotenv()
 
-sender = os.getenv("SENDER_EMAI")
+sender = os.getenv("SENDER_EMAIL")
 pass_word = os.getenv("PASSWORD")
 
 subject = "Hello there 🖐"
-df = pd.read_csv("email.csv")
-content = '''
-Just another spam email
-'''
+df = pd.read_csv("./emais.csv")
 
-def send_email(receiver):
+
+def send_email(receiver, content):
     yag = yagmail.SMTP(user=sender, password=pass_word)
     yag.send(to=receiver, subject=subject, contents=content)
     print("LMAO SPAMMED")
 
+for index, row in df.iterrows():
+    content = f"Hello there {row['name']}"
+    send_email(row['email'], content)
