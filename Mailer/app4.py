39d8c21959275dcc7b144cf26f9cedc84fb6b252
kamath7 <adithyakamath96@gmail.com>
2022-07-05 23:55:30 +0530

    sending emails with attachments

---
+++ Mailer/app4.py
@@ -0,0 +1,19 @@
+from json import load
+import yagmail
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+pass_word = os.getenv('PASSWORD')
+
+sender = os.getenv('SENDER_EMAIL')
+receiver = os.getenv('RECEIVER_EMAIL')
+
+subject = "Hello there 🖐"
+content = ["""
+Welcome to the awesome party at my place!
+""", 'doge.jpg']
+
+yag = yagmail.SMTP(user=sender, password=pass_word)
+yag.send(to=receiver, subject=subject, contents=content)
