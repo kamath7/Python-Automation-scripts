039801c341f688cecf0c2dcfeb540bf45e537cd6
kamath7 <adithyakamath96@gmail.com>
2022-07-07 02:07:36 +0530

    getting more data

--- pdfguy/app2.py
+++ pdfguy/app2.py
@@ -7,6 +7,15 @@ print(df)
 for index, row in df.iterrows():
     pdf = FPDF(orientation="P", unit = "pt", format="A4")
     pdf.add_page()
+
     pdf.set_font(family="Times", style="B",size=27)
     pdf.cell(w=0, h=50, txt=row["name"],align="C", border=1,ln=1)
+
+    for column in df.columns[1:]:
+
+        pdf.set_font(family="Times",size=14, style="B")
+        pdf.cell(w=100, h=20, txt=f"{column.title()}: ")
+        pdf.set_font(family="Times",size=14, style="B")
+        pdf.cell(w=100, h=20, txt=row[column], ln=1)
+
     pdf.output(f"{row['name']}.pdf")
