22f22ebf2ae2ce4d4a2c47f4d6034b54edc982a7
kamath7 <adithyakamath96@gmail.com>
2022-07-07 00:49:29 +0530

    species added

--- pdfguy/app.py
+++ pdfguy/app.py
@@ -12,8 +12,14 @@ pdf.cell(w=0, h=20, txt="Description", ln=1)
 
 pdf.set_font(family="Times", size=20)
 txt4 = """
-The Bengal tiger is a population of the Panthera tigris tigris subspecies It ranks among the biggest wild cats alive today. It is considered to belong to the world's charismatic megafauna
+The Bengal tiger is a population of the Panthera tigris  subspecies It ranks among the biggest wild cats alive today. It is considered to belong to the world's charismatic megafauna
 The tiger is estimated to have been present in the Indian subcontinent since the Late Pleistocene, for about 12,000 to 16,500 years.Today, it is threatened by poaching, loss and fragmentation of habitat, and was estimated at comprising fewer than 2,500 wild individuals by 2011. None of the Tiger Conservation Landscapes within its range is considered large enough to support an effective population of more than 250 adult individuals.
 """
 pdf.multi_cell(w=0, h=15, txt=txt4)
+
+pdf.set_font(family="Times",size=23, style="B")
+pdf.cell(w=100, h=20, txt="Species", ln=1)
+
+pdf.set_font(family="Times",size=23, style="B")
+pdf.cell(w=100, h=20, txt="Panthera tigris", ln=1)
 pdf.output('output1.pdf')
