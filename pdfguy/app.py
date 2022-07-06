bd3bf527cd6b94651ff4b546dd4da6082f78287d
kamath7 <adithyakamath96@gmail.com>
2022-07-07 00:39:50 +0530

    playing with text

--- pdfguy/app.py
+++ pdfguy/app.py
@@ -4,4 +4,6 @@ pdf = FPDF(orientation='portrait', unit='pt', format='A4')
 
 pdf.add_page()
 pdf.image('tiger.jpeg', w=80, h=50)
+pdf.set_font(family="Times", style="B",size=27)
+pdf.cell(w=0, h=50, txt="Awesome Tiger",align="C")
 pdf.output('output1.pdf')
