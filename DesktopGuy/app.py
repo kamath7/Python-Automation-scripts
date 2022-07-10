5d7838f097b89e7fb407c2d538f15bced61c0371
kamath7 <adithyakamath96@gmail.com>
2022-07-11 01:17:38 +0530

    easy app using pyqt

--- DesktopGuy/app.py
+++ DesktopGuy/app.py
@@ -1,5 +1,10 @@
 from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QPushButton, QLineEdit
 
+def make_sentence():
+    input_text = text.text()
+    output_label.setText(input_text.capitalize())
+    
+
 app = QApplication([])
 window = QWidget()
 window.setWindowTitle('Sentence Maker')
@@ -8,6 +13,15 @@ layout = QVBoxLayout()
 
 text = QLineEdit()
 layout.addWidget(text)
+
+btn = QPushButton("Awesome Button")
+layout.addWidget(btn)
+
+btn.clicked.connect(make_sentence)
+
+
+output_label = QLabel('')
+layout.addWidget(output_label)
 
 window.setLayout(layout)
 window.show()
