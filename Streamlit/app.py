9368e15129d4aa71304015b12b8ed6a002d827ea
kamath7 <adithyakamath96@gmail.com>
2022-07-11 00:05:10 +0530

    display table from dataframe

--- Streamlit/app.py
+++ Streamlit/app.py
@@ -1,7 +1,16 @@
 import streamlit as st 
+import pandas as pd 
+
+data = {
+    "Series_1":[1,3,4,5,7],
+    "Series_2":[10,60,80,90,100]
+}
+
+df = pd.DataFrame(data)
 
 st.title("First Streamlit")
 st.subheader("Streamlit is elite")
 st.write(""" 
 Awesome app!
 """)
+st.write(df)
