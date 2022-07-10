455c677bf0e2d205f5c2bc753fee3fcc9e9956b6
kamath7 <adithyakamath96@gmail.com>
2022-07-11 01:07:33 +0530

    adding a lyout

--- DesktopGuy/app.py
+++ DesktopGuy/app.py
@@ -1,7 +1,14 @@
 from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QPushButton, QLineEdit
 
-app = QApplication
+app = QApplication([])
 window = QWidget()
 window.setWindowTitle('Sentence Maker')
+
+layout = QVBoxLayout()
+
+text = QLineEdit()
+layout.addWidget(text)
+
+window.setLayout(layout)
 window.show()
 app.exec()
