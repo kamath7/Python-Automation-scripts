06312cec458b557fb5b7110038012094c4f4b5f4
kamath7 <adithyakamath96@gmail.com>
2022-07-11 01:04:31 +0530

    Basic PyQT

---
+++ DesktopGuy/app.py
@@ -0,0 +1,7 @@
+from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QPushButton, QLineEdit
+
+app = QApplication
+window = QWidget()
+window.setWindowTitle('Sentence Maker')
+window.show()
+app.exec()
