b892fac9c6a86b59df2dc67e691584bd8054a5ac
kamath7 <adithyakamath96@gmail.com>
2022-07-11 00:41:06 +0530

    changes to template

--- MoreWWebsites/app.py
+++ MoreWWebsites/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request
+import outcome
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
@@ -15,7 +17,7 @@ def homePostMethod():
 
     vol = float(length) * float(breadth) * float(height)
 
-    return render_template('index.html')
+    return render_template('index.html', output=vol, length=length, breadth=breadth, height=height)
 
 
 app.run("0.0.0.0")
