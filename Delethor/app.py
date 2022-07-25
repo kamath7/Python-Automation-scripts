cd51ea43ed04095d24a028f8e82d241349b9fe82
kamath7 <adithyakamath96@gmail.com>
2022-07-26 00:45:31 +0530

    func to delete

--- Delethor/app.py
+++ Delethor/app.py
@@ -1,5 +1,17 @@
-from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QPushButton
+from fileinput import filename
+from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QPushButton, QFileDialog
 from PyQt6.QtCore import Qt
+from pathlib import Path
+
+def open_files():
+    filenames, _ = QFileDialog.getOpenFileNames(window, "Select Files")
+    # print(filenames)
+    for filename in filenames:
+        path = Path(filename)
+        with open(path, 'wb') as file:
+            file.write(b'')
+        path.unlink()
+
 
 
 app = QApplication([])
@@ -15,7 +27,7 @@ open_btn = QPushButton("Open Files")
 open_btn.setToolTip("Open a bunch of files/file")
 open_btn.setFixedWidth(101)
 layout.addWidget(open_btn, alignment=Qt.AlignmentFlag.AlignCenter)
-
+open_btn.clicked.connect(open_files)
 
 window.setLayout(layout)
 window.show()
