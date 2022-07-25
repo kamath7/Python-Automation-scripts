db503bccf86d80189f5e84c234705267069857fc
kamath7 <adithyakamath96@gmail.com>
2022-07-26 00:32:55 +0530

    initial setup

---
+++ Delethor/app.py
@@ -0,0 +1,11 @@
+from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout
+
+app = QApplication()
+window = QWidget()
+window.setWindowTitle("Delethor")
+
+layout = QVBoxLayout()
+
+window.setLayout(layout)
+window.show()
+app.exec()
