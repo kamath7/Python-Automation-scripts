28dcabad1d14790c31775510ab8a417bbbe082e8
kamath7 <adithyakamath96@gmail.com>
2022-07-26 00:37:13 +0530

    adding tooltip

--- Delethor/app.py
+++ Delethor/app.py
@@ -1,4 +1,4 @@
-from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel
+from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QPushButton
 
 app = QApplication([])
 window = QWidget()
@@ -8,6 +8,11 @@ layout = QVBoxLayout()
 
 description = QLabel("Delethor helps you <font color=\"red\">delete permanently</font>")
 layout.addWidget(description)
+
+open_btn = QPushButton("Open Files")
+open_btn.setToolTip("Open a bunch of files/file")
+layout.addWidget(open_btn)
+
 
 window.setLayout(layout)
 window.show()
