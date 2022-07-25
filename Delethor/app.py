89d30127aa4ee307a1713a85f83c877a019cc085
kamath7 <adithyakamath96@gmail.com>
2022-07-26 00:35:34 +0530

    adding label

--- Delethor/app.py
+++ Delethor/app.py
@@ -1,10 +1,13 @@
-from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout
+from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel
 
 app = QApplication([])
 window = QWidget()
 window.setWindowTitle("Delethor")
 
 layout = QVBoxLayout()
+
+description = QLabel("Delethor helps you <font color=\"red\">delete permanently</font>")
+layout.addWidget(description)
 
 window.setLayout(layout)
 window.show()
