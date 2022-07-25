1b7630e5d6889c1d1ad322d97d092af53cd56fa5
kamath7 <adithyakamath96@gmail.com>
2022-07-26 00:54:52 +0530

    added message and app complete

--- Delethor/app.py
+++ Delethor/app.py
@@ -6,6 +6,7 @@ from pathlib import Path
 def open_files():
     global filenames
     filenames, _ = QFileDialog.getOpenFileNames(window, "Select Files")
+    message.setText('\n'.join(filenames))
     # print(filenames)
 
 
@@ -15,6 +16,7 @@ def destroy_files():
         with open(path, 'wb') as file:
             file.write(b'')
         path.unlink()
+    message.setText("Destroyed ⛏")
 
 
 
@@ -33,10 +35,13 @@ open_btn.setFixedWidth(101)
 layout.addWidget(open_btn, alignment=Qt.AlignmentFlag.AlignCenter)
 open_btn.clicked.connect(open_files)
 
-destroy_btn = QPushButton("Delete Selected Files")
+destroy_btn = QPushButton("Delete Files")
 destroy_btn.setFixedWidth(101)
 layout.addWidget(destroy_btn, alignment=Qt.AlignmentFlag.AlignCenter)
 destroy_btn.clicked.connect(destroy_files)
+
+message = QLabel(" ")
+layout.addWidget(message, alignment=Qt.AlignmentFlag.AlignCenter)
 
 window.setLayout(layout)
 window.show()
