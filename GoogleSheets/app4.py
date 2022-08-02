a42f76a028fd38da2a2ab8b0f20dbb82e165c813
kamath7 <adithyakamath96@gmail.com>
2022-08-02 23:53:01 +0530

    accessed cells

--- GoogleSheets/app4.py
+++ GoogleSheets/app4.py
@@ -1,4 +1,5 @@
 import gspread
+import re
 
 gc = gspread.service_account("iconic-iridium-341007-a5694e7775b8.json")
 spreadsheet = gc.open("Nicesheet")
@@ -8,5 +9,12 @@ worksheet1 = spreadsheet.worksheet("AwesomeSheet")
 cell2  = worksheet1.acell("B2").value 
 print(cell2)
 
-# cell3 = worksheet1.find('-10')
-# print(cell3.row,cell3.col)
+cell3 = worksheet1.find('77')
+print(cell3.row,cell3.col)
+cell11 = worksheet1.findall('88')
+print(cell11)
+
+#partial search
+
+regx = re.compile(r'9')
+cells = worksheet1.findall(regx)
