771faa698697ab27d5403af8b6061232e44b11fc
kamath7 <adithyakamath96@gmail.com>
2022-08-02 23:01:37 +0530

    working with google sheets

---
+++ GoogleSheets/app.py
@@ -0,0 +1,10 @@
+import pandas as pd 
+
+url_sheet1 = "https://docs.google.com/spreadsheets/d/1D7U4A9c-hwWWYokmGWAQnUTKsyvEmV9syig8NJuVa84/gviz/tq?tqx=out:csv&sheet=2013"
+url_sheet2 = "https://docs.google.com/spreadsheets/d/1D7U4A9c-hwWWYokmGWAQnUTKsyvEmV9syig8NJuVa84/gviz/tq?tqx=out:csv&sheet=2014"
+
+data = pd.read_csv(url_sheet1)
+data1 = pd.read_csv(url_sheet2)
+
+print(data)
+print(data1)
