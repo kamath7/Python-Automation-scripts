7e8fd9cef0abea8ec91a02846e08c1634716c480
kamath7 <adithyakamath96@gmail.com>
2022-08-02 23:55:15 +0530

    updating cells

---
+++ GoogleSheets/app5.py
@@ -0,0 +1,9 @@
+import gspread
+import re
+
+gc = gspread.service_account("iconic-iridium-341007-a5694e7775b8.json")
+spreadsheet = gc.open("Nicesheet")
+
+worksheet1 = spreadsheet.worksheet("AwesomeSheet")
+
+worksheet1.update('B2','299')
