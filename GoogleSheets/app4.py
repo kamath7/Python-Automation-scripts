aea98fd0f4bbc404dce1ae7706587030cad490db
kamath7 <adithyakamath96@gmail.com>
2022-08-02 23:50:28 +0530

    getting cell val

---
+++ GoogleSheets/app4.py
@@ -0,0 +1,12 @@
+import gspread
+
+gc = gspread.service_account("iconic-iridium-341007-a5694e7775b8.json")
+spreadsheet = gc.open("Nicesheet")
+
+worksheet1 = spreadsheet.worksheet("AwesomeSheet")
+
+cell2  = worksheet1.acell("B2").value 
+print(cell2)
+
+# cell3 = worksheet1.find('-10')
+# print(cell3.row,cell3.col)
