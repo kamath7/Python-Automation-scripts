3824386bf901c09847f4d006c5b523ad3f10eded
kamath7 <adithyakamath96@gmail.com>
2022-08-02 23:18:23 +0530

    getting worksheet data

--- GoogleSheets/app2.py
+++ GoogleSheets/app2.py
@@ -3,3 +3,10 @@ import gspread
 gc = gspread.service_account("iconic-iridium-341007-a5694e7775b8.json")
 spreadsheet = gc.open("Nicesheet")
 
+worksheet1 = spreadsheet.get_worksheet(0)
+data = worksheet1.get_all_records()
+print(data)
+
+worksheet2 = spreadsheet.worksheet("AwesomeSheet2")
+data1 = worksheet2.get_all_records()
+print(data1)
