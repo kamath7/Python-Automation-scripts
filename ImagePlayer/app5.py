073d81533009e1cd07670f250a65befefce2d3d4
kamath7 <adithyakamath96@gmail.com>
2022-08-04 02:36:21 +0530

    face detection done

--- ImagePlayer/app5.py
+++ ImagePlayer/app5.py
@@ -5,3 +5,9 @@ face_cascade = cv2.CascadeClassifier('faces.xml')
 
 faces = face_cascade.detectMultiScale(image, 1.1, 4)
 print(faces)
+
+for (x,y,w,h) in faces:
+    #x,y - coords
+    cv2.rectangle(image, (x,y), (x+w, y+h), (255,0,0), 3) #x+w = first corner of the rectangle to the end 
+    
+cv2.imwrite('humanf.jpeg',image)
