771c2bf8d8c2ea68749e17e67eca09a9923b4e9d
kamath7 <adithyakamath96@gmail.com>
2022-08-04 02:21:45 +0530

    resizing batch

---
+++ ImagePlayer/app4.py
@@ -0,0 +1,20 @@
+import cv2, os
+
+def calc_size(scale_percentage, width, height):
+    new_width = int(round(width * scale_percentage / 100))
+    new_height = int(round(height * scale_percentage / 100))
+    return (new_width, new_height)
+
+def resize(img_path, scale_percentage,resize_path="./"):
+    image = cv2.imread(img_path)
+    new_dim = calc_size(scale_percentage, image.shape[1], image.shape[0])
+    resized_image = cv2.resize(image, new_dim)
+    cv2.imwrite(resize_path,resized_image)
+
+
+li_of_imgs = os.listdir('./images/')
+
+for img in li_of_imgs:
+    resize(f'images/{img}',200,f'/resized/oorlalle{img}')
+    
+
