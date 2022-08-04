ff24518f7cd1d81ec75b24d69a42154e91b56d3b
kamath7 <adithyakamath96@gmail.com>
2022-08-04 20:45:11 +0530

    better than photoshop

--- ImagePlayer/app8.py
+++ ImagePlayer/app8.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 foreground = cv2.imread('./giraffe.jpeg')
 background = cv2.imread('./safari.jpeg')
@@ -7,13 +8,14 @@ width = foreground.shape[1]
 height = foreground.shape[0]
 
 # print(foreground(40, 40))
+resized_bg = cv2.resize(background, (width, height))
 
 for i in range(width):
     for j in range(height):
         pixel = foreground[j,i]
 
-        if list(pixel) == [28, 255, 76]:
-            foreground[j,i] = background[j,i]
+        if np.any(pixel == [1, 255, 0]) :
+            foreground[j,i] = resized_bg[j,i]
 
 
 cv2.imwrite('op.jpeg', foreground)
