7523d0ba93bb858b2c8097743d8aa51b820d61ca
kamath7 <adithyakamath96@gmail.com>
2022-08-24 02:12:46 +0530

    making it stronger

--- RegexPlayer/app.py
+++ RegexPlayer/app.py
@@ -1,6 +1,6 @@
 import re
 
-pattern = re.compile('[a-z]+@[a-z]+.[a-z]+')
+pattern = re.compile('[^ ]+@[a-z]+.[a-z]+')
 
 files = ['1.txt','2.txt','aa.txt']
 emails = "this is lalle. oorlalea@a.com and another meail b@b.com"
