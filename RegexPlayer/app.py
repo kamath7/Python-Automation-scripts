5d39c4ac8df6a9902c9b24d2e366a5308f083770
kamath7 <adithyakamath96@gmail.com>
2022-08-24 02:07:29 +0530

    getting email address

---
+++ RegexPlayer/app.py
@@ -0,0 +1,9 @@
+import re
+
+pattern = re.compile('[a-z]+@[a-z]+.[a-z]+')
+
+files = ['1.txt','2.txt','aa.txt']
+emails = "this is lalle. oorlalea@a.com and another meail b@b.com"
+
+matches = pattern.findall(emails)
+print(matches)
