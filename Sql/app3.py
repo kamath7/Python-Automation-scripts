b903015d2ff72a2fa6f5c777091ca005f4c6ad1a
kamath7 <adithyakamath96@gmail.com>
2022-08-31 02:01:25 +0530

    pdf generated

--- Sql/app3.py
+++ Sql/app3.py
@@ -13,5 +13,14 @@ pdf.add_page()
 for col in cols:
     pdf.set_font(family="Times", style="B", size=14)
     pdf.cell(w=100, h=30, txt=col[1], border=1)
-    
+
+pdf.ln()
+curson.execute("SELECT * FROM ips")
+rows = curson.fetchall()
+
+for row in rows:
+    for elem in row:
+        pdf.set_font(family="Times", size=14)
+        pdf.cell(w=100, h=10, txt=str(elem), border=1)
+    pdf.ln()
 pdf.output("db.pdf")
