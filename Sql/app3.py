b04c7e614611ce99583d1c0f131b4b5b37776c4c
kamath7 <adithyakamath96@gmail.com>
2022-08-31 01:57:23 +0530

    pdf print for md

--- Sql/app3.py
+++ Sql/app3.py
@@ -7,5 +7,11 @@ curson = con.cursor()
 curson.execute("PRAGMA table_info(ips)")
 cols = curson.fetchall()
 
+pdf = FPDF(orientation="P", unit= "pt", format="A4")
+pdf.add_page()
+
 for col in cols:
-    print(col[1])
+    pdf.set_font(family="Times", style="B", size=14)
+    pdf.cell(w=100, h=30, txt=col[1], border=1)
+    
+pdf.output("db.pdf")
