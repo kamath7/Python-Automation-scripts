ab219a7c6291a371cba77f8b6a807d30a64d29b6
kamath7 <adithyakamath96@gmail.com>
2022-08-31 01:39:17 +0530

    where clause

--- Sql/app.py
+++ Sql/app.py
@@ -9,3 +9,6 @@ print(cursor.fetchall())
 
 cursor.execute("SELECT address,asn FROM 'ips' ORDER BY asn")
 print(cursor.fetchall())
+
+cursor.execute("SELECT * FROM 'ips' WHERE asn < 300")
+print(cursor.fetchall())
