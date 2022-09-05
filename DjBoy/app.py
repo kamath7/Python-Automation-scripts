3ff4fdc5b9543d68503283912a31751b77e3e4c5
kamath7 <adithyakamath96@gmail.com>
2022-09-05 20:41:40 +0530

    adding a silence

--- DjBoy/app.py
+++ DjBoy/app.py
@@ -11,3 +11,9 @@ first_two = original[0:2000] #first 2 secs
 first_two.export('./2sec.wav')
 
 print(len(original)) #3567 -> 3.5 second
+
+merge = original + reversed
+merge.export('merged.wav')
+
+merge1 = original + AudioSegment.silent(1000) +  reversed
+merge1.export('merged1.wav')
