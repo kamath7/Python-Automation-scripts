13e296b59a05a175c630e084010b5d8e1a7ab995
kamath7 <adithyakamath96@gmail.com>
2022-09-08 00:29:59 +0530

    setting up flask

--- WebcamStream/app.py
+++ WebcamStream/app.py
@@ -1,4 +1,5 @@
 import cv2
+from flask import Flask, render_template, Response
 
 video = cv2.VideoCapture(0)
 
@@ -7,3 +8,11 @@ def get_frame():
     sc, encoded_image = cv2.imencode('.jpg', frame)
     frame = encoded_image.tobytes()
     yield(b'--frame\r\nContent-Type: image/jpeg\r\n'+frame+b'\r\n')
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
