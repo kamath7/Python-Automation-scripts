b738a45ab8eecc5761bb8155e5e867477cc880e2
kamath7 <adithyakamath96@gmail.com>
2022-09-08 02:21:11 +0530

    good amt of stats

--- HArdware/app.py
+++ HArdware/app.py
@@ -4,3 +4,9 @@ print(psutil.cpu_count(logical=False))
 print(psutil.cpu_percent(interval=5))
 print(psutil.cpu_times())
 print(psutil.cpu_stats())
+
+print(psutil.virtual_memory())
+print(psutil.swap_memory())
+
+print(psutil.disk_usage('/').percent)
+print(psutil.disk_partitions)
